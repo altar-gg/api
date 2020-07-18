e9dd04a1fb9c1fc6f994866888010252884bea29
rubybb <10256477+rubybb@users.noreply.github.com>
2020-07-18 10:19:53 -0600

    further implementation on account api

--- src/routes/account.js
+++ src/routes/account.js
@@ -1,33 +1,43 @@
 module.exports = function (context) {
     const app = context.express();
 
+    const ratelimit = require("../middleware/ratelimit");
+    const captcha = require("../middleware/captcha");
+    const auth = require("../middleware/auth");
+    const flag = require("../middleware/flag");
+
     /* 
         POST /v1/account/
         RATELIMIT, CAPTCHA, NO_AUTH
 
         Create new account.
-    */
-    app.post("/", async (req, res) => {
+    */ 
+    app.post("/", ratelimit({
+        every: "24 hours", 
+        use: "manual"
+    }), captcha(), auth(false), 
+
+    async (req, res) => {
         res.json({});        
     });
 
     /* 
         GET /v1/account/
-        AUTHED, FLAG("VIEW_SELF")
+        AUTHED, FLAG("VIEW_SELF"), RATELIMIT
 
         Obtain currently logged in account.
     */
-    app.get("/", async (req, res) => {
+    app.get("/", ratelimit(), auth(), flag("VIEW_SELF"), async (req, res) => {
         res.json({});
     });
 
     /* 
-        GET /v1/account/edit/
+        POST /v1/account/edit/
         AUTHED, FLAG("MODIFY_SELF"), CAPTCHA, RATELIMIT
 
         Modify currently logged in account.
     */
-    app.get("/edit/", async (req, res) => {
+    app.post("/edit/", ratelimit(), captcha(), auth(), flag("MODIFY_SELF"), async (req, res) => {
         res.json({});
     });
 
@@ -37,7 +47,7 @@ module.exports = function (context) {
 
         Delete current account.
     */
-    app.delete("/", async (req, res) => {
+    app.delete("/", captcha(), auth(), flag("DELETE_SELF"), async (req, res) => {
         res.json({});        
     });
 
@@ -47,17 +57,19 @@ module.exports = function (context) {
 
         Get another user account.
     */
-    app.get("/:id/", async (req, res) => {
+    app.get("/:id/", ratelimit(), auth(), async (req, res) => {
         res.json({});        
     });
 
     /* 
-        GET /v1/account/:id/
+        POST /v1/account/:id/
         AUTHED, FLAG("MODIFY_OTHER_ACCOUNTS"), CAPTCHA, RATELIMIT
 
         Modify another user account.
     */
-    app.get("/:id/edit/", async (req, res) => {
+
+    
+    app.post("/:id/edit/", ratelimit(), captcha(), auth(), flag("MODIFY_OTHER_ACCOUNTS"), async (req, res) => {
         res.json({});        
     });
 
@@ -67,7 +79,7 @@ module.exports = function (context) {
 
         Delete another user account.
     */
-    app.get("/:id/", async (req, res) => {
+    app.delete("/:id/", captcha(), auth(), flag("DELETE_OTHER_ACCOUNTS"), async (req, res) => {
        res.json({});        
     });
    
